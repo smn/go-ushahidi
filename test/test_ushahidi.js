2c0839aa54c192c81790b18dcbe9246b8228764d
Simon de Haan <simon@praekeltfoundation.org>
2013-02-21 05:28:27 +0200

    tests!

--- test/test_ushahidi.js
+++ test/test_ushahidi.js
@@ -13,3 +13,167 @@ describe("test_api", function() {
         assert.ok(app.api.on_inbound_event);
     });
 });
+
+function reset_im(im) {
+    im.user = null;
+    im.i18n = null;
+    im.i18n_lang = null;
+    im.current_state = null;
+}
+
+function fresh_api() {
+    var api = app.api;
+    api.reset();
+    reset_im(api.im);
+    return api;
+}
+
+function maybe_call(f, that, args) {
+    if (typeof f != "undefined" && f !== null) {
+        f.apply(that, args);
+    }
+}
+
+function check_state(user, content, next_state, expected_response, setup,
+                     teardown) {
+    // setup api
+    var api = fresh_api();
+    var from_addr = "1234567";
+    var user_key = "users." + from_addr;
+    api.kv_store[user_key] = user;
+
+    maybe_call(setup, this, [api]);
+
+    api.add_reply({
+        cmd: "outbound.reply_to"
+    });
+
+    // send message
+    api.on_inbound_message({
+        cmd: "inbound-message",
+        msg: {
+            from_addr: from_addr,
+            content: content,
+            message_id: "123"
+        }
+    });
+
+    // check result
+    var saved_user = api.kv_store[user_key];
+    assert.equal(saved_user.current_state, next_state);
+    var reply = api.request_calls.shift();
+    var response = reply.content;
+    try {
+        assert.ok(response);
+        assert.ok(response.match(expected_response));
+        assert.ok(response.length <= 163);
+    } catch (e) {
+        console.log(api.logs);
+        console.log(response);
+        console.log(expected_response);
+        if (typeof response != 'undefined')
+            console.log("Content length: " + response.length);
+        throw e;
+    }
+    assert.deepEqual(app.api.request_calls, []);
+    assert.equal(app.api.done_calls, 1);
+
+    maybe_call(teardown, this, [api, saved_user]);
+}
+
+function check_close(user, next_state, setup, teardown) {
+    var api = fresh_api();
+    var from_addr = "1234567";
+    var user_key = "users." + from_addr;
+    api.kv_store[user_key] = user;
+
+    maybe_call(setup, this, [api]);
+
+    // send message
+    api.on_inbound_message({
+        cmd: "inbound-message",
+        msg: {
+            from_addr: from_addr,
+            session_event: "close",
+            content: "User Timeout",
+            message_id: "123"
+        }
+    });
+
+    // check result
+    var saved_user = api.kv_store[user_key];
+    assert.equal(saved_user.current_state, next_state);
+    assert.deepEqual(app.api.request_calls, []);
+    assert.equal(app.api.done_calls, 1);
+
+    maybe_call(teardown, this, [api, saved_user]);
+}
+
+function CustomTester(custom_setup, custom_teardown) {
+    var self = this;
+
+    self._combine_setup = function(custom_setup, orig_setup) {
+        var combined_setup = function (api) {
+            maybe_call(custom_setup, self, [api]);
+            maybe_call(orig_setup, this, [api]);
+        };
+        return combined_setup;
+    };
+
+    self._combine_teardown = function(custom_teardown, orig_teardown) {
+        var combined_teardown = function (api, saved_user) {
+            maybe_call(custom_teardown, self, [api, saved_user]);
+            maybe_call(orig_teardown, this, [api, saved_user]);
+        };
+        return combined_teardown;
+    };
+
+    self.check_state = function(user, content, next_state, expected_response,
+                                setup, teardown) {
+        return check_state(user, content, next_state, expected_response,
+                           self._combine_setup(custom_setup, setup),
+                           self._combine_teardown(custom_teardown, teardown));
+    };
+
+    self.check_close = function(user, next_state, setup, teardown) {
+        return check_close(user, next_state,
+                           self._combine_setup(custom_setup, setup),
+                           self._combine_teardown(custom_teardown, teardown));
+    };
+}
+
+describe("test_ussd_states_for_session_1", function() {
+    it("new users should see the intro state", function () {
+        check_state(null, null, "intro", "^Select a category");
+    });
+    it("reply 'foo' to intro should produce error text", function() {
+        check_state({current_state: "intro"}, "foo", "intro",
+            "^Please select a valid category.[^]"
+        );
+    });
+    it("reply '1' to intro should go to title", function() {
+        check_state({current_state: "intro"}, "1", "report_title",
+            "^What is the title?"
+        );
+    });
+    it("reply 'title' to report_title should go to description", function() {
+        check_state({current_state: "report_title"}, "the title",
+            "report_description",
+            "^What is the description?"
+        );
+    });
+    it("reply 'description' to report_description should go to location",
+        function() {
+            check_state({current_state: "report_description"}, "the description",
+                "report_location",
+                "^Please type in the address"
+           );
+        });
+    it("reply 'address' to report_location should end the session",
+        function() {
+            check_state({current_state: "report_location"}, "the address",
+                "submit_report",
+                "^Thank you, your report has been submitted"
+            );
+        });
+});
